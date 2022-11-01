e9962d4a040987baf72044a145e61a075dee9fbe
Dinagar <69046908+Dinagar-dev@users.noreply.github.com>
2022-11-01 11:35:51 +0530

    Delete PatientRepo.java

--- PatientRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.healthcareapi.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.healthcareapi.model.Patients;
-
-public interface PatientRepo extends JpaRepository<Patients,Long>{
-
-}
